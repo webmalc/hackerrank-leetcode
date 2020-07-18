dafd037329fb269c93b05dbba9648062aa4859a6
webmalc <m@webmalc.pw>
2020-07-18 22:13:30 +0300

    Add the count triplets challenge

---
+++ count_triplets_1/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// https://www.hackerrank.com/challenges/count-triplets-1
+func countTriplets(arr []int, r int) int {
+	left := make(map[int]int)
+	right := make(map[int]int)
+	result := 0
+
+	for _, v := range arr {
+		right[v]++
+	}
+	for _, v := range arr {
+		right[v]--
+		if v%r == 0 {
+			result += left[v/r] * right[v*r]
+		}
+		left[v]++
+	}
+
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nr := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(nr[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	r, err := strconv.ParseInt(nr[1], 10, 64)
+	checkError(err)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int
+
+	for i := 0; i < int(n); i++ {
+		arrItem, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arr = append(arr, int(arrItem))
+	}
+
+	ans := countTriplets(arr, int(r))
+
+	fmt.Fprintf(writer, "%d\n", ans)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ count_triplets_1/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCountTriplets(t *testing.T) {
+	cases := []struct {
+		result   int
+		expected int
+	}{
+		{countTriplets([]int{1, 2, 2, 4, 5}, 2), 2},
+		{countTriplets([]int{1, 3, 9, 9, 27, 81}, 3), 6},
+		{countTriplets([]int{1, 5, 5, 25, 125}, 5), 4},
+		{countTriplets([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 1), 161700},
+	}
+	for _, c := range cases {
+		if c.expected != c.result {
+			t.Errorf("Error got: %d, want: %d.", c.result, c.expected)
+		}
+	}
+}
